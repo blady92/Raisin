2f0c6a7b2eb7abc93cde908da2b7cbe70ee43447
v3l0c1r4pt0r <v3l0c1r4pt0r@gmail.com>
2015-03-16 19:58:21 +0100

    Zacząłem prace z timerem (zegarem)

---
+++ Testy/Kamil Lorenc/Cyber2O/Cyber2O/Cyber2O/Clock.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Cyber2O
+{
+    [Serializable]
+    public class Clock
+    {
+        private DateTime starttime;
+        private static volatile Clock instance;
+        private static object syncRoot = new Object();
+        SortedDictionary<int, TickEventHandler> eventQueue;
+
+        public delegate void TickEventHandler(object sender, int time);
+        private event TickEventHandler TickEvent;
+
+    private Clock() 
+    {
+        starttime = DateTime.Now;
+        eventQueue = new SortedDictionary<int, TickEventHandler>();
+        throw new NotImplementedException("TODO: add infinite wait in new thread");
+    }
+
+    public void AddEvent(int time, TickEventHandler toDo)
+    {
+        eventQueue.Add(time,toDo);
+    }
+
+   public static Clock Instance
+   {
+      get 
+      {
+         if (instance == null) 
+         {
+            lock (syncRoot) 
+            {
+               if (instance == null)
+                   instance = new Clock();
+            }
+         }
+
+         return instance;
+      }
+   }
+    }
+}
---
+++ Testy/Kamil Lorenc/Cyber2O/Cyber2OTests/ClockTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Cyber2O;
+
+namespace Cyber2OTests
+{
+    [TestClass]
+    public class ClockTests
+    {
+        [TestMethod]
+        public void ShouldCallHandlerAfterZeroMinutes()
+        {
+            //given
+            Clock clock = Clock.Instance;
+            //when
+            clock.AddEvent(0, TickEventHandler);
+            //then
+            Assert.Fail("Not expected to get here");
+        }
+
+        private void TickEventHandler(object sender, int time)
+        {
+            throw new NotImplementedException("tik, tak, tik, tak");
+        }
+    }
+}
