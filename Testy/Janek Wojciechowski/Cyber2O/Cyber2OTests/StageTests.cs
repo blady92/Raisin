1f8c9acffdad9ec5200dfd7c6909c6a2248272a1
blady92 <blady992@gmail.com>
2015-04-10 12:15:36 +0200

    [TEST] Dodałem generowanie labiryntu i jego wstępną strukturę

---
+++ Testy/Janek Wojciechowski/Cyber2O/Cyber2OTests/StageTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Cyber2O;
+
+namespace Cyber2OTests
+{
+    [TestClass]
+    public class StageTests
+    {
+        [TestMethod]
+        public void TestParseBitmapSquare()
+        {
+            StageParser parser = new StageParser();
+            Stage stage = parser.ParseBitmap("../../Assets/preview.bmp");
+            Assert.IsNotNull(stage);
+            Assert.IsTrue(stage.Objects.Count == 1);
+            Assert.IsTrue(stage.NPCs.Count == 2);
+            Assert.IsTrue(stage.Rooms.Count == 3);
+            Assert.IsTrue(stage.Corridors.Count == 2);
+        }
+
+        [TestMethod]
+        public void TestParseBitmapNotSquare()
+        {
+            StageParser parser = new StageParser();
+            Stage stage = parser.ParseBitmap("../../Assets/preview2.bmp");
+            Assert.IsNotNull(stage);
+            Assert.IsTrue(stage.Objects.Count == 2);
+            Assert.IsTrue(stage.NPCs.Count == 1);
+            Assert.IsTrue(stage.Rooms.Count == 3);
+            Assert.IsTrue(stage.Corridors.Count == 1);
+        }
+    }
+}
