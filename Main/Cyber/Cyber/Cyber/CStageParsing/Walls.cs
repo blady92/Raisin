46d4ce3798e6ff12c51599a9f50806df492c373c
blady92 <blady992@gmail.com>
2015-05-07 03:31:26 +0200

    [MAIN] Dodano parsowanie korytarzy do labiryntu

--- Main/Cyber/Cyber/Cyber/CStageParsing/Walls.cs
+++ Main/Cyber/Cyber/Cyber/CStageParsing/Walls.cs
@@ -27,6 +27,30 @@ namespace Cyber.CStageParsing
             WallsLeft = new List<Pair<int, int>>();
             WallsRight = new List<Pair<int, int>>();
 
+            bool [,] structure = new bool [stage.Width, stage.Height];
+
+            foreach (Room room in stage.Rooms)
+            {
+                for (int h = 0; h < room.Height; h++)
+                {
+                    for (int w = 0; w < room.Width; w++)
+                    {
+                        structure[w, h] |= room.Structure[w, h];
+                    }
+                }
+            }
+
+            foreach (Corridor corridor in stage.Corridors)
+            {
+                for (int h = 0; h < corridor.Height; h++)
+                {
+                    for (int w = 0; w < corridor.Width; w++)
+                    {
+                        structure[w, h] |= corridor.Structure[w, h];
+                    }
+                }
+            }
+            /*
             foreach (Room room in stage.Rooms)
             {
                 bool [,] structure = room.Structure;
@@ -52,6 +76,32 @@ namespace Cyber.CStageParsing
                             {
                                 WallsDown.Add(new Pair<int, int>(w, h));
                             }
+                        }
+                    }
+                }
+            }
+            */
+            for (int h = 0; h < stage.Height; h++)
+            {
+                for (int w = 0; w < stage.Width; w++)
+                {
+                    if (isTrue(structure, w, h))
+                    {
+                        if (!isTrue(structure, w - 1, h))
+                        {
+                            WallsLeft.Add(new Pair<int, int>(w, h));
+                        }
+                        if (!isTrue(structure, w + 1, h))
+                        {
+                            WallsRight.Add(new Pair<int, int>(w, h));
+                        }
+                        if (!isTrue(structure, w, h - 1))
+                        {
+                            WallsUp.Add(new Pair<int, int>(w, h));
+                        }
+                        if (!isTrue(structure, w, h + 1))
+                        {
+                            WallsDown.Add(new Pair<int, int>(w, h));
                         }
                     }
                 }
