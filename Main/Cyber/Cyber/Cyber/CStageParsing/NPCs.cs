9c0df9ae01f485f8bf1b674c79b3c9d507ae6e0c
blady92 <blady992@gmail.com>
2015-05-15 03:06:12 +0200

    [LABIRYNT] Parsowanie obiektów wstępnie działa, ale coś nie chce wyświetlać terminala :/

---
+++ Main/Cyber/Cyber/Cyber/CStageParsing/NPCs.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cyber.CStageParsing
+{
+    public class Tank : StageNPC
+    {
+        public Tank(int height, int width) : base(height, width) { }
+        public override string StaticObjectAsset
+        {
+            get { return "Assets/3D/Characters/Ally_Bunker"; }
+        }
+
+        public override IGenerable clone()
+        {
+            IGenerable instance = new Tank(Height, Width);
+            instance.Structure = (bool[,])Structure.Clone();
+            return instance;
+        }
+    }
+
+    public class Spy : StageNPC
+    {
+        public Spy(int height, int width) : base(height, width) { }
+        public override string StaticObjectAsset
+        {
+            get { return "Assets/3D/Characters/Ally_Bunker"; }
+        }
+
+        public override IGenerable clone()
+        {
+            IGenerable instance = new Spy(Height, Width);
+            instance.Structure = (bool[,])Structure.Clone();
+            return instance;
+        }
+    }
+
+    public class Flyer : StageNPC
+    {
+        public Flyer(int height, int width) : base(height, width) { }
+        public override string StaticObjectAsset
+        {
+            get { return "Assets/3D/Characters/Ally_Bunker"; }
+        }
+
+        public override IGenerable clone()
+        {
+            IGenerable instance = new Flyer(Height, Width);
+            instance.Structure = (bool[,])Structure.Clone();
+            return instance;
+        }
+    }
+}
