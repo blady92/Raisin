e784a0545de67a486d146443b57ce28feb626966
blady92 <blady992@gmail.com>
2015-05-20 18:13:11 +0200

    Dodano utilsy do Stage'a

---
+++ Main/Cyber/Cyber/Cyber/CLogicEngine/StageUtils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cyber.CLogicEngine
+{
+    public class StageUtils
+    {
+        public static const double PRZESUNIECIE = 19.5d; 
+        public static double BitmapCoordToStageCoord(double coord)
+        {
+            return coord * PRZESUNIECIE;
+        }
+
+        public static double[] BitmapCoordsToStageCoords(params double[] coords)
+        {
+            double[] result = new double[coords.Length];
+            for (int i = 0; i < coords.Length; i++)
+            {
+                result[i] = BitmapCoordToStageCoord(coords[i]);
+            }
+            return result;
+        }
+    }
+}
